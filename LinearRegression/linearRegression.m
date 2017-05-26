599d8b3200306d1c1be2c1ebe1b115b88ac8a74d
Ionesio Junior <ionesiojr@gmail.com>
2017-05-25 22:53:43 -0300

    Added Features Scaling

--- LinearRegression/linearRegression.m
+++ LinearRegression/linearRegression.m
@@ -6,6 +6,15 @@ theta = ones(size(data)(2),1);
 alpha = str2num(arg_list{1});
 it = str2num(arg_list{2});
 sum_distance = [];
+max_elements = [];
+
+% Feature Scaling
+for i = 2:size(data)(2)
+	max_elements(end + 1) = max(data(:,i));
+	theta(i:1) = theta(i,1) / max(data(:,i));
+	data(:,i) = data(:,i) / max(data(:,i));
+end
+
 
 for i = 0:it
 	hipotesys = data * theta;
@@ -17,6 +26,11 @@ for i = 0:it
 		sum_result = (sum(matrix(:)) / size(data)(1)) * alpha;
 		theta(j,1) = theta(j,1) - sum_result;
 	end
+end
+
+% Scaling theta results
+for i = 2:size(data)(2)
+	theta(i,1) = theta(i,1) / max_elements(i - 1);
 end
 	range = 0:1:it;
 	hold on;
